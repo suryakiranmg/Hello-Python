d22830d3e82f386ab822c3f303f9a18e65b37b20
Suryakiran M. George <25682779+suryakiranmg@users.noreply.github.com>
2019-01-29 01:16:00 +0530

    Update hellopython_2.py

--- hellopython_2.py
+++ hellopython_2.py
@@ -2,8 +2,7 @@ import random
 import sys
 import os
 
-grocery_list = ['Juice','Tomatoes','Potatoes',
-                'Bananas']
+grocery_list = ['Juice','Tomatoes','Potatoes', 'Bananas']
 print('First Item is',grocery_list[0])
 
 grocery_list[0]="Green Juice"
@@ -31,3 +30,22 @@ print(to_do_list_2)
 print(len(to_do_list_2))
 print("Max is",max(to_do_list_2))
 print("Min is",min(to_do_list_2))
+
+
+####---------~~~~~~~~~~~~~~~------RESULTS-----------~~~~~~~~~~~~~~~~~~~~~~------###
+
+First Item is Juice
+First Item is Green Juice
+
+ ['Tomatoes', 'Potatoes']
+
+ [['Wash Car', 'Pick up Kids', 'Cash Check'], ['Green Juice', 'Tomatoes', 'Potatoes', 'Bananas']]
+
+Tomatoes
+[['Wash Car', 'Pick up Kids', 'Cash Check'], ['Green Juice', 'Tomatoes', 'Potatoes', 'Bananas', 'Onions']]
+[['Wash Car', 'Pick up Kids', 'Cash Check'], ['Tomatoes', 'Potatoes', 'Onions', 'Green Juice']]
+['Wash Car', 'Pick up Kids', 'Cash Check', 'Tomatoes', 'Potatoes', 'Onions', 'Green Juice']
+7
+Max is Wash Car
+Min is Cash Check
+
